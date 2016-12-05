1b762458cdab934a2bfaa0adc8972a7ce553510b
Ryan@windows10 <goluckyryan@gmail.com>
2016-12-05 16:39:59 +0900

    improve bat script

--- Analysis_UI/deploying_Analysis.bat
+++ Analysis_UI/deploying_Analysis.bat
@@ -2,24 +2,30 @@
 title Deploying Analysis Program
 echo =====================================================
 echo This is batch script for deploying Analysis in winodws
+Set desktop=%USERPROFILE%\Desktop
+echo Desktop PATH: %Desktop%
 echo =====================================================
 pause
 
 Set name=Analysis_UI.exe
 Set fftwLib=libfftw3-3_x64.dll
-Set origin=C:\Users\Triplet-ESR\Desktop\nonLinearFit\build-Analysis_UI-Desktop_Qt_5_7_0_MSVC2015_64bit-Release\release
-Set destination=C:\Users\Triplet-ESR\Desktop\Analysis_Release
+Set origin=%Desktop%\nonLinearFit\build-Analysis_UI-Desktop_Qt_5_7_0_MSVC2015_64bit-Release\release
+Set destination=%Desktop%\Analysis_Release
 rem Set destination=C:\Users\Triplet-ESR\Desktop\kaka
 Set compiler=C:\Qt\5.7\msvc2015_64\bin\
 
 rem -----------------------------------------
+echo =====================================================
 echo Copy *.exe to %destination%
+echo =====================================================
 xcopy %origin%\%name% %destination%
 xcopy %origin%\%fftwLib% %destination%
 pause
 
 rem -----------------------------------------
-set /p deployFlag="Deploy: Y/N?"
+echo =====================================================
+set /p deployFlag="  Deploy Application? (Y/N)"
+echo =====================================================
 
 IF "%deployFlag%"=="Y" (
 	rem -----------------------------------------
